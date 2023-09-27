49bbb7268845dfc246e45a4a43f9c015bd0f529d
gongchanghua <759444207@qq.com>
2023-09-27 22:45:48 +0800

    feature(state): implement the KeyedValueState in the KeyedValueStateDemo

--- src/main/java/com/ddsr/state/KeyedValueStateDemo.java
+++ src/main/java/com/ddsr/state/KeyedValueStateDemo.java
@@ -3,6 +3,10 @@ package com.ddsr.state;
 import com.ddsr.bean.WaterSensor;
 import com.ddsr.functions.WaterSensorMapFunc;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
@@ -30,8 +34,35 @@ public class KeyedValueStateDemo {
 
         sensorDS.keyBy(WaterSensor::getId)
                 .process(new KeyedProcessFunction<String, WaterSensor, String>() {
+                    // Define a value state, based on the key, which is only initialized in the open method
+                    ValueState<Integer> lastVcState;
+
                     @Override
-                    public void processElement(WaterSensor value, KeyedProcessFunction<String, WaterSensor, String>.Context ctx, Collector<String> out) throws Exception {
+                    public void open(Configuration parameters) throws Exception {
+                        super.open(parameters);
+                        // the name of state, must be unique; the type of state
+                        lastVcState = getRuntimeContext().getState(new ValueStateDescriptor<>("lastVcState", Types.INT));
+                    }
+
+                    @Override
+                    public void processElement(WaterSensor value, Context ctx, Collector<String> out) throws Exception {
+
+//                        lastVcState.value() ; // get the value of lastVcState
+//                        lastVcState.update(0); // update the value of lastVcState
+//                        lastVcState.clear(); // clear the value of lastVcState
+
+                        // get the value of the state, 0 if null(the default value of Integer class is null)
+                        int lastVc = lastVcState.value()  == null ? 0 : lastVcState.value();
+
+                        // if the different value between contiguous data is more than 10, then print
+                        Integer vc = value.getVc();
+                        if (Math.abs(vc - lastVc) > 10) {
+                            out.collect("传感器=" + value.getId() + "==>当前水位值=" + vc + ",与上一条水位值=" + lastVc + ",相差超过10！！！！");
+                        }
+
+                        // store the value into the state
+                        lastVcState.update(vc);
+
 
                     }
                 })
