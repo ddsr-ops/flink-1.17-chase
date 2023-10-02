761c0e2a27e7b127659c3aa02fdbd4be100187d1
gongchanghua <759444207@qq.com>
2023-10-02 21:50:14 +0800

    feature(checkpoint): add some senior configurations of checkpoint

--- src/main/java/com/ddsr/checkpoint/CheckpointConfigDemo.java
+++ src/main/java/com/ddsr/checkpoint/CheckpointConfigDemo.java
@@ -1,17 +1,11 @@
 package com.ddsr.checkpoint;
 
-import com.ddsr.bean.WaterSensor;
-import com.ddsr.functions.WaterSensorMapFunc;
-import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.state.StateTtlConfig;
-import org.apache.flink.api.common.state.ValueState;
-import org.apache.flink.api.common.state.ValueStateDescriptor;
-import org.apache.flink.api.common.time.Time;
 import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.util.Collector;
 
 import java.time.Duration;
@@ -21,60 +15,52 @@ import java.time.Duration;
  */
 public class CheckpointConfigDemo {
     public static void main(String[] args) throws Exception {
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
         env.setParallelism(1);
 
+        // 代码中用到hdfs，需要导入hadoop依赖、指定访问hdfs的用户名
+        System.setProperty("HADOOP_USER_NAME", "hive");
 
-        SingleOutputStreamOperator<WaterSensor> sensorDS = env
+        // 检查点配置
+        // 1、启用检查点: 默认是barrier对齐的，周期为5s, 精准一次
+        env.enableCheckpointing(5000, CheckpointingMode.EXACTLY_ONCE);
+        CheckpointConfig checkpointConfig = env.getCheckpointConfig();
+        // 2、指定检查点的存储位置
+        checkpointConfig.setCheckpointStorage("hdfs://hadoop102:8020/chk");
+        // 3、checkpoint的超时时间: 默认10分钟
+        checkpointConfig.setCheckpointTimeout(60000);
+        // 4、同时运行中的checkpoint的最大数量
+        checkpointConfig.setMaxConcurrentCheckpoints(1);
+        // 5、最小等待间隔: 上一轮checkpoint结束 到 下一轮checkpoint开始 之间的间隔，设置了>0,并发就会变成1
+        checkpointConfig.setMinPauseBetweenCheckpoints(1000);
+        // 6、取消作业时，checkpoint的数据 是否保留在外部系统
+        // DELETE_ON_CANCELLATION:主动cancel时，删除存在外部系统的chk-xx目录 （如果是程序突然挂掉，不会删）
+        // RETAIN_ON_CANCELLATION:主动cancel时，外部系统的chk-xx目录会保存下来
+        checkpointConfig.setExternalizedCheckpointCleanup(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+        // 7、允许 checkpoint 连续失败的次数，默认0--》表示checkpoint一失败，job就挂掉
+        checkpointConfig.setTolerableCheckpointFailureNumber(10);
+
+        //  开启 非对齐检查点（barrier非对齐）
+        // 开启的要求： Checkpoint模式必须是精准一次，最大并发必须设为1
+        checkpointConfig.enableUnalignedCheckpoints();
+        // 开启非对齐检查点才生效： 默认0，表示一开始就直接用 非对齐的检查点
+        // 如果大于0， 一开始用 对齐的检查点（barrier对齐）， 对齐的时间超过这个参数，自动切换成 非对齐检查点（barrier非对齐）
+        checkpointConfig.setAlignedCheckpointTimeout(Duration.ofSeconds(1));
+
+
+        env
                 .socketTextStream("ora11g", 7777)
-                .map(new WaterSensorMapFunc())
-                .assignTimestampsAndWatermarks(
-                        WatermarkStrategy
-                                .<WaterSensor>forBoundedOutOfOrderness(Duration.ofSeconds(3))
-                                .withTimestampAssigner((element, ts) -> element.getTs() * 1000L)
-                );
-
-        sensorDS.keyBy(WaterSensor::getId)
-                .process(
-                        new KeyedProcessFunction<String, WaterSensor, String>() {
-
-                            ValueState<Integer> lastVcState;
-
-
-                            @Override
-                            public void open(Configuration parameters) throws Exception {
-                                super.open(parameters);
-
-                                // 1.创建 StateTtlConfig
-                                StateTtlConfig stateTtlConfig = StateTtlConfig
-                                        .newBuilder(Time.seconds(5)) // 过期时间5s
-//                                        .setUpdateType(StateTtlConfig.UpdateType.OnCreateAndWrite) // 状态 创建和写入（更新） 更新 过期时间
-                                        .setUpdateType(StateTtlConfig.UpdateType.OnReadAndWrite) // 状态 读取、创建和写入（更新） 更新 过期时间
-                                        .setStateVisibility(StateTtlConfig.StateVisibility.NeverReturnExpired) // 不返回过期的状态值， 一般来说使用这个选项值
-                                        .build();
-
-                                //  2.状态描述器 启用 TTL
-                                ValueStateDescriptor<Integer> stateDescriptor = new ValueStateDescriptor<>("lastVcState", Types.INT);
-                                stateDescriptor.enableTimeToLive(stateTtlConfig);
-
-
-                                this.lastVcState = getRuntimeContext().getState(stateDescriptor);
-
-                            }
-
-                            @Override
-                            public void processElement(WaterSensor value, Context ctx, Collector<String> out) throws Exception {
-                                // 先获取状态值，打印 ==》 读取状态
-                                Integer lastVc = lastVcState.value();
-                                out.collect("key=" + value.getId() + ",状态值=" + lastVc);
-
-                                // 如果水位大于10，更新状态值 ===》 写入状态
-                                if (value.getVc() > 10) {
-                                    lastVcState.update(value.getVc());
-                                }
+                .flatMap(
+                        (String value, Collector<Tuple2<String, Integer>> out) -> {
+                            String[] words = value.split(" ");
+                            for (String word : words) {
+                                out.collect(Tuple2.of(word, 1));
                             }
                         }
                 )
+                .returns(Types.TUPLE(Types.STRING, Types.INT))
+                .keyBy(value -> value.f0)
+                .sum(1)
                 .print();
 
         env.execute();
