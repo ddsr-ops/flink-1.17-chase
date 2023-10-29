352051c837793b9c8f4b278e7846d2f3c9c78331
gongchanghua <759444207@qq.com>
2023-10-29 22:04:23 +0800

    feature(flink-sql): add an example for demonstrating a create-like ddl

--- src/main/java/com/ddsr/sql/ddl/table.sql
+++ src/main/java/com/ddsr/sql/ddl/table.sql
@@ -24,3 +24,49 @@ CREATE TABLE MyTable (
       'connector' = 'kafka'
       ...
       );
+
+-- 主键约束表明表中的一列或一组列是唯一的，并且它们不包含NULL值。主键唯一地标识表中的一行，只支持 not enforced。
+CREATE TABLE MyTable (
+                         `user_id` BIGINT,
+                         `name` STRING,
+                         PARYMARY KEY(user_id) not enforced -- PRIMARY KEY must not be nullable
+) WITH (
+      'connector' = 'kafka'
+      ...
+      );
+
+-- PRIMARY KEY #
+-- Primary key constraint is a hint for Flink to leverage for optimizations. It tells that a column or a set of columns of a table or a view are unique and they do not contain null. Neither of columns in a primary can be nullable. Primary key therefore uniquely identify a row in a table.
+--
+-- Primary key constraint can be either declared along with a column definition (a column constraint) or as a single line (a table constraint). For both cases, it should only be declared as a singleton. If you define multiple primary key constraints at the same time, an exception would be thrown.
+--
+-- Validity Check
+--
+-- SQL standard specifies that a constraint can either be ENFORCED or NOT ENFORCED. This controls if the constraint checks are performed on the incoming/outgoing data. Flink does not own the data therefore the only mode we want to support is the NOT ENFORCED mode. It is up to the user to ensure that the query enforces key integrity.
+--
+-- Flink will assume correctness of the primary key by assuming that the columns nullability is aligned with the columns in primary key. Connectors should ensure those are aligned.
+--
+-- Notes: In a CREATE TABLE statement, creating a primary key constraint will alter the columns nullability, that means, a column with primary key constraint is not nullable.
+
+
+CREATE TABLE Orders (
+                        `user` BIGINT,
+                        product STRING,
+                        order_time TIMESTAMP(3)
+) WITH (
+      'connector' = 'kafka',
+      'scan.startup.mode' = 'earliest-offset'
+      );
+
+CREATE TABLE Orders_with_watermark (
+    -- Add watermark definition
+    WATERMARK FOR order_time AS order_time - INTERVAL '5' SECOND
+) WITH (
+      -- Overwrite the startup-mode
+      'scan.startup.mode' = 'latest-offset'
+      )
+    LIKE Orders;
+
+-- 用于基于现有表的定义创建表。此外，用户可以扩展原始表或排除表的某些部分。
+-- 可以使用该子句重用(可能还会覆盖)某些连接器属性，或者向外部定义的表添加水印。
+-- 可增加属性（原表没有的属性），覆盖属性（原表有的属性）。
