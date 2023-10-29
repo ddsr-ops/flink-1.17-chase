de615fe1fb70b2c40de3b55e7e2b8b4bccf7288c
gongchanghua <759444207@qq.com>
2023-10-29 20:11:01 +0800

    feature(flink-sql): add a kafka-create table for demonstrating how to use metadata columns

---
+++ src/main/java/com/ddsr/sql/ddl/table-kafka.sql
@@ -0,0 +1,52 @@
+-- https://nightlies.apache.org/flink/flink-docs-release-1.18/docs/connectors/table/kafka/
+
+CREATE TABLE KafkaTable (
+                            `event_time` TIMESTAMP_LTZ(3) METADATA FROM 'timestamp', -- R/W 'timestamp' is Timestamp of the Kafka record.
+                            `partition` BIGINT METADATA VIRTUAL, -- Only readable permitted, must be written explicitly here
+                            `offset` BIGINT METADATA VIRTUAL, -- Only readable permitted, must be written explicitly here
+                            `user_id` BIGINT,
+                            `item_id` BIGINT,
+                            `behavior` STRING
+) WITH (
+      'connector' = 'kafka',
+      'topic' = 'user_behavior',
+      'properties.bootstrap.servers' = 'localhost:9092',
+      'properties.group.id' = 'testGroup',
+      'scan.startup.mode' = 'earliest-offset',
+      'format' = 'csv'
+      );
+
+-- What metadata columns are writable or only readable, see the reference of the first line
+-- If customized column name is the same as the metadata column name , `from metadata-column` is unnecessary to be written.
+-- For example, column `offset` is defined column in KafkaTable, so `from offset`(offset is a metadata column name) is not needed to be written.
+
+CREATE TABLE MyTable (
+                         `user_id` BIGINT,
+                         `name` STRING,
+-- 将时间戳强转为 BIGINT
+                         `timestamp` BIGINT METADATA -- convert timestamp to bigint
+) WITH (
+      'connector' = 'kafka'
+      ...
+      );
+-- 如果自定义列的数据类型和 Connector 中定义的 metadata 字段的数据类型不一致，程序运行时会自动 cast强转，但是这要求两种数据类型是可以强转的。
+
+
+CREATE TABLE KafkaTable (
+                            `event_time` TIMESTAMP_LTZ(3) METADATA FROM 'value.source.timestamp' VIRTUAL,  -- from Debezium format
+                            `origin_table` STRING METADATA FROM 'value.source.table' VIRTUAL, -- from Debezium format
+                            `partition_id` BIGINT METADATA FROM 'partition' VIRTUAL,  -- from Kafka connector
+                            `offset` BIGINT METADATA VIRTUAL,  -- from Kafka connector
+                            `user_id` BIGINT,
+                            `item_id` BIGINT,
+                            `behavior` STRING
+) WITH (
+      'connector' = 'kafka',
+      'topic' = 'user_behavior',
+      'properties.bootstrap.servers' = 'localhost:9092',
+      'properties.group.id' = 'testGroup',
+      'scan.startup.mode' = 'earliest-offset',
+      'value.format' = 'debezium-json'
+      );
+
+--
--- src/main/java/com/ddsr/sql/ddl/table.sql
+++ src/main/java/com/ddsr/sql/ddl/table.sql
@@ -8,3 +8,8 @@ CREATE TABLE [IF NOT EXISTS] [catalog_name.][db_name.]table_name
     [PARTITIONED BY (partition_column_name1, partition_column_name2, ...)]
     WITH (key1=val1, key2=val2, ...)
     [ LIKE source_table [( <like_options> )] | AS select_query ]
+
+
+-- metadata_column_definition
+-- 元数据列是 SQL 标准的扩展，允许访问数据源本身具有的一些元数据。元数据列由 METADATA 关键字标识。
+-- 具体示例，参考table-kafka.sql
