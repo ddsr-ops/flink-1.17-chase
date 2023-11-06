111abe4225a562d80ca831fb2e99c138a756137a
gongchanghua <759444207@qq.com>
2023-11-06 22:15:41 +0800

    feature(flink-sql): add a cumulate window sql demo

---
+++ src/main/java/com/ddsr/sql/cumulate.sql
@@ -0,0 +1,8 @@
+SELECT
+    window_start,
+    window_end,
+    id ,
+    SUM(vc) sumVC
+FROM TABLE(
+        CUMULATE(TABLE ws, DESCRIPTOR(et), INTERVAL '2' SECONDS , INTERVAL '6' SECONDS))
+GROUP BY window_start, window_end, id;
