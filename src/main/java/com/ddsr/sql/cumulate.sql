2f5ad50abffc9ff2c1a696b1ee1ec341c80aba20
gongchanghua <759444207@qq.com>
2023-11-07 21:47:11 +0800

    feature(flink-sql): add a cumulate window sql demo with multiple dimensions analysis

--- src/main/java/com/ddsr/sql/cumulate.sql
+++ src/main/java/com/ddsr/sql/cumulate.sql
@@ -6,3 +6,6 @@ SELECT
 FROM TABLE(
         CUMULATE(TABLE ws, DESCRIPTOR(et), INTERVAL '2' SECONDS , INTERVAL '6' SECONDS))
 GROUP BY window_start, window_end, id;
+--  rollup( (id) )
+--  cube( (id) )
+--  grouping sets( (id),()  )
