95ab380e47f11b16a925cd39b9fdd4df8d836c57
gongchanghua <759444207@qq.com>
2023-11-09 08:51:55 +0800

    feature(flink-sql): add a aggregation-over sql demonstration, supplying some examples

--- src/main/java/com/ddsr/sql/agg_over.sql
+++ src/main/java/com/ddsr/sql/agg_over.sql
@@ -1,10 +1,26 @@
-SELECT
-    agg_func(agg_col) OVER (
+SELECT agg_func(agg_col) OVER (
     [PARTITION BY col1[, col2, ...]]
     ORDER BY time_col
     range_definition),
   ...
-FROM ...
+FROM ...;
 --     ORDER BY：必须是时间戳列（事件时间、处理时间），只能升序
 --     PARTITION BY：标识了聚合窗口的聚合粒度
---     range_definition：这个标识聚合窗口的聚合数据范围，在 Flink 中有两种指定数据范围的方式。第一种为按照行数聚合，第二种为按照时间区间聚合
+--     range_definition：这个标识聚合窗口的聚合数据范围，在 Flink 中有两种指定数据范围的方式。
+--     第一种为按照行数(rows)聚合，第二种为按照时间区间(range)聚合
+
+
+-- range interval
+-- Note 2 things： the unit is second, the '5' is string literal
+select id,
+       et,
+       vc,
+       count(vc) over(partition by id order by et range between interval '5' second preceding and current row) as vc2
+from ws;
+
+-- rows interval
+select id,
+       et,
+       vc,
+       count(vc) over(partition by id order by et rows between 5 preceding and current row) as vc2
+from ws;
