c84fa98e8ae0eeb56d8dbf74dd02d250f7723bd8
gongchanghua <759444207@qq.com>
2023-11-13 21:39:26 +0800

    feature(flink-sql): add a regula join sql demonstration, additional docs

--- src/main/java/com/ddsr/sql/regular_join.sql
+++ src/main/java/com/ddsr/sql/regular_join.sql
@@ -17,3 +17,6 @@ SELECT *
 FROM ws
          FULL OUTER JOIN ws1
                          ON ws.id = ws.id;
+
+-- The states(left table or right table) are hold in the memory, will be cleared if they exceed the TTL
+-- Therefore, the data might be lost after TTL is expired, setting a suitable TTL is recommended
