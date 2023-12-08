dd5fe6e9691f2ea5369eacfca0176f9d6e744e7f
gongchanghua <759444207@qq.com>
2023-12-08 23:22:50 +0800

    feature(cep): first cep demo of process function

--- src/main/java/com/ddsr/cep/FirstCep.java
+++ src/main/java/com/ddsr/cep/FirstCep.java
@@ -1,45 +1,30 @@
 package com.ddsr.cep;
 
 import org.apache.flink.cep.CEP;
-import org.apache.flink.cep.PatternSelectFunction;
 import org.apache.flink.cep.PatternStream;
+import org.apache.flink.cep.functions.PatternProcessFunction;
 import org.apache.flink.cep.pattern.Pattern;
 import org.apache.flink.cep.pattern.conditions.SimpleCondition;
 import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
 /**
  * @author ddsr, created it at 2023/12/7 22:43
  */
 public class FirstCep {
-    public static void main(String[] args) {
-
-//        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        // todo: initialize a socket data stream from host 88.88.16.126 with port 7777
-
-        // todo: Define a CEP pattern, the length of the first string is 3, the next string starts with 'b'
-
-        // todo: apply the CEP pattern to the socket data stream
-
-        // todo: fetch result applied CEP pattern , then print it
-
-        // print "I am out"
-        System.out.println("I am out");
-
-
-
-    }
-
-    public static void CEP(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        DataStream<String> socketDataStream = env.socketTextStream("88.88.16.126", 7777);
+        env.setParallelism(1);
+
+        DataStream<String> socketDataStream = env.socketTextStream("192.168.20.126", 7777);
+
+        socketDataStream.print();
 
         Pattern<String, ?> cepPattern = Pattern.<String>begin("first")
                 .where(new SimpleCondition<String>() {
@@ -58,15 +43,33 @@ public class FirstCep {
 
         PatternStream<String> patternStream = CEP.pattern(socketDataStream, cepPattern);
 
-        DataStream<String> resultStream = patternStream.select(new PatternSelectFunction<String, String>() {
-            @Override
-            public String select(Map<String, List<String>> pattern) {
-                return pattern.get("first").get(0) + " " + pattern.get("second").get(0);
-            }
-        });
+        // use process function to fetch result alternatively instead of select method
+
+        DataStream<String> resultStream = patternStream.process(
+                new PatternProcessFunction<String, String>() {
+                    @Override
+                    public void processMatch(
+                            Map<String, List<String>> pattern,
+                            Context ctx,
+                            Collector<String> out) throws Exception {
+                        String result = pattern.get("first").get(0)
+                                + " "
+                                + pattern.get("second").get(0);
+                        out.collect(result);
+                    }
+                });
+//        DataStream<String> resultStream = patternStream.select(new PatternSelectFunction<String, String>() {
+//            @Override
+//            public String select(Map<String, List<String>> pattern) {
+//                return pattern.get("first").get(0) + " " + pattern.get("second").get(0);
+//            }
+//        });
 
         resultStream.print();
 
         env.execute();
+
+
     }
+
 }
