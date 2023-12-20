bd133c9f617f97db1a30ccb6183d8f3bb44582a0
gongchanghua <759444207@qq.com>
2023-12-20 22:18:16 +0800

    feature(cep): GroupPattern of followBy

--- src/main/java/com/ddsr/cep/group/GroupsOfPatterns.java
+++ src/main/java/com/ddsr/cep/group/GroupsOfPatterns.java
@@ -25,25 +25,39 @@ public class GroupsOfPatterns {
 
         DataStreamSource<String> ds = env.socketTextStream("192.168.20.126", 7777);
 
-        // GroupPattern of begin, comment the following patterns after this pattern if testing
+        // GroupPattern of begin, comment the following patterns after this pattern if testing, and apply the start pattern
+        // to the ds stream
         // Test case: a c bx by, output: a bx
-        GroupPattern<String, String> pattern = Pattern.begin(
+        GroupPattern<String, String> start = Pattern.begin(
                 Pattern.<String>begin("start1")
                         .where(SimpleCondition.of(s -> s.startsWith("a")))
                         .followedBy("middle1")
                         .where(SimpleCondition.of(s -> s.startsWith("b")))
         );
 
-        // GroupPattern of next, comment the following patterns after this pattern if testing
+        // GroupPattern of next
         // Test case: a c b1 3 c1 4 d2 a c b1 c 2 d1, output: {start1=[a], middle1=[b1], start2=[c], middle2=[d1]}
         // Note: the '3' breaks the next match, so no output for a b1 c1 d2
-        pattern = pattern
+        GroupPattern<String, String> pattern = start
                 .next(
                         Pattern.<String>begin("start2")
                                 .where(SimpleCondition.of(s -> s.startsWith("c")))
                                 .followedBy("middle2")
                                 .where(SimpleCondition.of(s -> s.startsWith("d")))
                 );
+
+        // GroupPattern of followedBy
+        // Test case: a c b1 3 c1 4 d2 a c b1 c 2 d1,
+        // Output: {start1=[a], middle1=[b1], start3=[c1], middle3=[d2]}
+        // {start1=[a], middle1=[b1], start3=[c], middle3=[d1]}
+        pattern = start
+                .followedBy(
+                        Pattern.<String>begin("start3")
+                                .where(SimpleCondition.of(s -> s.startsWith("c")))
+                                .followedBy("middle3")
+                                .where(SimpleCondition.of(s -> s.startsWith("d")))
+                );
+
 
         PatternStream<String> patternStream = CEP.pattern(ds, pattern).inProcessingTime();
 
