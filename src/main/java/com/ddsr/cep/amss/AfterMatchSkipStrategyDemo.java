62bd240a5abb026141dc6e00bd96546f1b8fa340
gongchanghua <759444207@qq.com>
2023-12-21 22:44:21 +0800

    feature(cep): init AfterMatchSkipStrategyDemo

---
+++ src/main/java/com/ddsr/cep/amss/AfterMatchSkipStrategyDemo.java
@@ -0,0 +1,10 @@
+package com.ddsr.cep.amss;
+
+/**
+ * @author ddsr, created it at 2023/12/21 22:43
+ */
+public class AfterMatchSkipStrategyDemo {
+    public static void main(String[] args) {
+
+    }
+}
