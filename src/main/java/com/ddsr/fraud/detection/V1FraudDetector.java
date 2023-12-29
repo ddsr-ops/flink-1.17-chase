fdf90a845726fb447a91473467b9a8b80d8de654
gongchanghua <759444207@qq.com>
2023-12-29 22:40:46 +0800

    feature(fraud-detect): Implement process of V1FraudDetector

--- src/main/java/com/ddsr/fraud/detection/V1FraudDetector.java
+++ src/main/java/com/ddsr/fraud/detection/V1FraudDetector.java
@@ -3,6 +3,7 @@ package com.ddsr.fraud.detection;
 import javafx.scene.control.Alert;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.configuration.Configuration;
 
 /**
  * @author ddsr, created it at 2023/12/29 22:30
@@ -13,6 +14,8 @@ public class V1FraudDetector extends KeyedProcessFunction<Long, Transaction, Ale
 
     private transient ValueState<Boolean> flagState;
 
+
+
     @Override
     public void open(Configuration parameters) {
         ValueStateDescriptor<Boolean> flagDescriptor = new ValueStateDescriptor<>(
@@ -20,3 +23,33 @@ public class V1FraudDetector extends KeyedProcessFunction<Long, Transaction, Ale
                 Types.BOOLEAN);
         flagState = getRuntimeContext().getState(flagDescriptor);
     }
+
+    @Override
+    public void processElement(
+            Transaction transaction,
+            Context context,
+            Collector<Alert> collector) throws Exception {
+
+        // Get the current state for the current key
+        Boolean lastTransactionWasSmall = flagState.value();
+
+        // Check if the flag is set
+        if (lastTransactionWasSmall != null) {
+            if (transaction.getAmount() > LARGE_AMOUNT) {
+                // Output an alert downstream
+                Alert alert = new Alert();
+                alert.setId(transaction.getAccountId());
+
+                collector.collect(alert);
+            }
+
+            // Clean up our state
+            flagState.clear();
+        }
+
+        if (transaction.getAmount() < SMALL_AMOUNT) {
+            // Set the flag to true
+            flagState.update(true);
+        }
+    }
+}
