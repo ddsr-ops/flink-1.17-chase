2ce32155b7fb3fc9a0354f65221e1c81b09b287e
gongchanghua <759444207@qq.com>
2023-12-29 22:41:40 +0800

    feature(fraud-detect): Finalize the FraudDetector

--- src/main/java/com/ddsr/fraud/detection/V1FraudDetector.java
+++ src/main/java/com/ddsr/fraud/detection/V1FraudDetector.java
@@ -1,20 +1,24 @@
 package com.ddsr.fraud.detection;
 
-import javafx.scene.control.Alert;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.util.Collector;
+import org.apache.flink.walkthrough.common.entity.Alert;
+import org.apache.flink.walkthrough.common.entity.Transaction;
 
-/**
- * @author ddsr, created it at 2023/12/29 22:30
- */
-public class V1FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert> {
+public class FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert> {
 
     private static final long serialVersionUID = 1L;
 
+    private static final double SMALL_AMOUNT = 1.00;
+    private static final double LARGE_AMOUNT = 500.00;
+    private static final long ONE_MINUTE = 60 * 1000;
+
     private transient ValueState<Boolean> flagState;
-
-
+    private transient ValueState<Long> timerState;
 
     @Override
     public void open(Configuration parameters) {
@@ -22,6 +26,11 @@ public class V1FraudDetector extends KeyedProcessFunction<Long, Transaction, Ale
                 "flag",
                 Types.BOOLEAN);
         flagState = getRuntimeContext().getState(flagDescriptor);
+
+        ValueStateDescriptor<Long> timerDescriptor = new ValueStateDescriptor<>(
+                "timer-state",
+                Types.LONG);
+        timerState = getRuntimeContext().getState(timerDescriptor);
     }
 
     @Override
@@ -36,20 +45,41 @@ public class V1FraudDetector extends KeyedProcessFunction<Long, Transaction, Ale
         // Check if the flag is set
         if (lastTransactionWasSmall != null) {
             if (transaction.getAmount() > LARGE_AMOUNT) {
-                // Output an alert downstream
+                //Output an alert downstream
                 Alert alert = new Alert();
                 alert.setId(transaction.getAccountId());
 
                 collector.collect(alert);
             }
-
             // Clean up our state
-            flagState.clear();
+            cleanUp(context);
         }
 
         if (transaction.getAmount() < SMALL_AMOUNT) {
-            // Set the flag to true
+            // set the flag to true
             flagState.update(true);
+
+            long timer = context.timerService().currentProcessingTime() + ONE_MINUTE;
+            context.timerService().registerProcessingTimeTimer(timer);
+
+            timerState.update(timer);
         }
+    }
+
+    @Override
+    public void onTimer(long timestamp, OnTimerContext ctx, Collector<Alert> out) {
+        // remove flag after 1 minute
+        timerState.clear();
+        flagState.clear();
+    }
+
+    private void cleanUp(Context ctx) throws Exception {
+        // delete timer
+        Long timer = timerState.value();
+        ctx.timerService().deleteProcessingTimeTimer(timer);
+
+        // clean up all state
+        timerState.clear();
+        flagState.clear();
     }
 }
