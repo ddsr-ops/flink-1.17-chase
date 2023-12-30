3b55e967b4f27faec6051ed4472071de5db639ee
gongchanghua <759444207@qq.com>
2023-12-30 19:40:24 +0800

    feature(fraud-detect): Add a Transaction Class

--- src/main/java/com/ddsr/fraud/detection/V1FraudDetector.java
+++ src/main/java/com/ddsr/fraud/detection/V1FraudDetector.java
@@ -1,15 +1,14 @@
 package com.ddsr.fraud.detection;
 
+import javafx.scene.control.Alert;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.util.Collector;
-import org.apache.flink.walkthrough.common.entity.Alert;
-import org.apache.flink.walkthrough.common.entity.Transaction;
 
-public class FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert> {
+public class V1FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert> {
 
     private static final long serialVersionUID = 1L;
 
