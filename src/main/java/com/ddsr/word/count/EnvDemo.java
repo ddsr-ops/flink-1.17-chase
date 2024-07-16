50e1cd4fdabbb91892d2db2abe92e164aca3462b
gongchanghua <759444207@qq.com>
2024-07-16 18:11:59 +0800

    chore(demo): add suppress warnings for Convert2Lambda

--- src/main/java/com/ddsr/word/count/EnvDemo.java
+++ src/main/java/com/ddsr/word/count/EnvDemo.java
@@ -12,7 +12,7 @@ import org.apache.flink.util.Collector;
  * @author ddsr, created it at 2023/8/12 22:24
  */
 public class EnvDemo {
-    @SuppressWarnings("deprecation")
+    @SuppressWarnings({"deprecation", "Convert2Lambda"})
     public static void main(String[] args) throws Exception {
         Configuration configuration = new Configuration();
         // rest web ui port
