018de899718a8b00ac88607d9276947fa8c424cc
gongchanghua <759444207@qq.com>
2024-09-03 06:54:33 +0800

    fix(pom): remove duplicated flink-connector-files dependency

---
+++ src/test/java/com/ddsr/testing/udf/IncrementMapFunctionTest.java
@@ -0,0 +1,23 @@
+package com.ddsr.testing.udf;
+
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test {@link IncrementMapFunction}
+ *
+ * @author ddsr, created it at 2024/9/3 6:35
+ */
+public class IncrementMapFunctionTest {
+
+    @Test
+    public void testIncrement() throws Exception {
+        IncrementMapFunction incrementMapFunction = new IncrementMapFunction();
+
+        assertEquals(Optional.of(2L), incrementMapFunction.map(1L));
+
+    }
+}
