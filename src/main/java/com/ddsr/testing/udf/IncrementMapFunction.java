e19cd384d940981472115f5c084ae272ce805060
gongchanghua <759444207@qq.com>
2024-09-03 06:31:32 +0800

    feature(test): Add a map udf to test flink-test-utils

---
+++ src/main/java/com/ddsr/testing/udf/IncrementMapFunction.java
@@ -0,0 +1,17 @@
+package com.ddsr.testing.udf;
+
+import org.apache.flink.api.common.functions.MapFunction;
+
+/**
+ * Unit testing stateless, timeless udfs using flink-test-utils
+ *
+ *
+ * @author ddsr, created it at 2024/9/2 18:28
+ */
+public class IncrementMapFunction implements MapFunction<Long, Long> {
+
+    @Override
+    public Long map(Long record) throws Exception {
+        return record + 1;
+    }
+}
