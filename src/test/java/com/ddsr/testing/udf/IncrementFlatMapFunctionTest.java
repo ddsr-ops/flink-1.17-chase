7667a4d8030c6ed79d6a7537e2c4a5db1d4a0f2f
gongchanghua <759444207@qq.com>
2024-09-09 06:54:49 +0800

    feature(test): add a dependency of mockito

---
+++ src/test/java/com/ddsr/testing/udf/IncrementFlatMapFunctionTest.java
@@ -0,0 +1,16 @@
+package com.ddsr.testing.udf;
+
+import org.mockito.Mockito;
+
+/**
+ * @author ddsr, created it at 2024/9/9 6:47
+ */
+public class IncrementFlatMapFunctionTest {
+
+    @Test
+    public void testIncrement() throws Exception {
+        IncrementFlatMapFunction incrementFlatMapFunction = new IncrementFlatMapFunction();
+
+        mock(Collector.class)
+
+}
