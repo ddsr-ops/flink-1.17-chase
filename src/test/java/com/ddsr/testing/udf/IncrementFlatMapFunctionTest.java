30d7fb44451569bd3f8a3726a2fa83903ac94095
gongchanghua <759444207@qq.com>
2024-09-12 18:27:06 +0800

    feature(test): add test class for a flatmap udf

--- src/test/java/com/ddsr/testing/udf/IncrementFlatMapFunctionTest.java
+++ src/test/java/com/ddsr/testing/udf/IncrementFlatMapFunctionTest.java
@@ -1,8 +1,24 @@
 package com.ddsr.testing.udf;
 
+import org.apache.flink.util.Collector;
+import org.junit.Test;
 import org.mockito.Mockito;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+
 /**
+ *
+ * The following dependency should be added in the pom.xml
+ * <blockquote><pre>
+ *         <dependency>
+ *             <groupId>org.mockito</groupId>
+ *             <artifactId>mockito-core</artifactId>
+ *             <version>3.4.6</version> <!-- the version should be consistent with the version of corresponding mockito dependency in flink-test-utils -->
+ *             <scope>test</scope>
+ *         </dependency>
+ * </pre></blockquote>
+ *
  * @author ddsr, created it at 2024/9/9 6:47
  */
 public class IncrementFlatMapFunctionTest {
@@ -11,6 +27,13 @@ public class IncrementFlatMapFunctionTest {
     public void testIncrement() throws Exception {
         IncrementFlatMapFunction incrementFlatMapFunction = new IncrementFlatMapFunction();
 
-        mock(Collector.class)
+        Collector<Long> collector = mock(Collector.class);
+
+        // call the methods that have been implemented
+        incrementFlatMapFunction.flatMap(3L, collector);
+
+        // todo: what does the times mean?
+        Mockito.verify(collector, times(1)).collect(3L);
+    }
 
 }
