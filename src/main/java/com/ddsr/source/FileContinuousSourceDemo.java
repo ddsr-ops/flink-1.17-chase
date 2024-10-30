881a8cfd4b0bc70cedf9f277eadce8a6b80d1fb7
gongchanghua <759444207@qq.com>
2024-10-30 17:27:36 +0800

    feature(source): update file continuous reading

--- src/main/java/com/ddsr/source/FileContinuousSourceDemo.java
+++ src/main/java/com/ddsr/source/FileContinuousSourceDemo.java
@@ -1,9 +1,17 @@
 package com.ddsr.source;
 
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.connector.source.Boundedness;
 import org.apache.flink.api.java.io.TextInputFormat;
+import org.apache.flink.connector.file.src.FileSource;
+import org.apache.flink.connector.file.src.reader.TextLineInputFormat;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.source.FileProcessingMode;
+
+import java.time.Duration;
+
+import static java.time.temporal.ChronoUnit.SECONDS;
 
 /**
  * @author ddsr, created it at 2024/1/25 22:00
@@ -11,10 +19,29 @@ import org.apache.flink.streaming.api.functions.source.FileProcessingMode;
 public class FileContinuousSourceDemo {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        env.enableCheckpointing(1000);
 
         // Read the files in the input directory continuously
-        env.readFile(new TextInputFormat(new Path("input")), "input",
-                FileProcessingMode.PROCESS_CONTINUOUSLY, 1000)
+        // readFile api has been deprecated
+        // If the monitored files are modified, the corresponding files are re-processed from head to end
+//        env.readFile(new TextInputFormat(new Path("input")), "input",
+//                FileProcessingMode.PROCESS_CONTINUOUSLY, 1000)
+//                .print();
+
+        FileSource<String> source =
+             FileSource.forRecordStreamFormat(
+                           new TextLineInputFormat(), new Path("input"))
+             .monitorContinuously(Duration.of(1, SECONDS))
+                        .build();
+
+        Boundedness boundedness = source.getBoundedness();
+
+//         Read the files in the input directory continuously, when files are modified, modification should be found
+        // This implementation can not find the modification, whether the file is modified or files are added, the
+        // cause can not addressed at 2024-10-30 17:26:59
+        env.fromSource(source, WatermarkStrategy.noWatermarks(), "fs")
                 .print();
 
         env.execute();
