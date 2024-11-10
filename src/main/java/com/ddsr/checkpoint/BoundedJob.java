d495b17e3a1e8a764d3f390eec33447fe6271bd1
gongchanghua <759444207@qq.com>
2024-11-10 16:51:40 +0800

    feature(checkpoint): improve the bounded job

--- src/main/java/com/ddsr/checkpoint/BoundedJob.java
+++ src/main/java/com/ddsr/checkpoint/BoundedJob.java
@@ -1,29 +1,69 @@
 package com.ddsr.checkpoint;
 
+import org.apache.flink.api.common.RuntimeExecutionMode;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 
 import java.util.Properties;
 
+/**
+ * One obvious outlier is when you want to use a bounded job to bootstrap some job state that you then want to use in an
+ * unbounded job. For example, by running a bounded job using STREAMING mode, taking a savepoint, and then restoring
+ * that savepoint on an unbounded job.
+ *
+ * @see {@link UnboundedJob}
+ */
+@SuppressWarnings({"deprecation"})
 public class BoundedJob {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+        env.setRuntimeMode(RuntimeExecutionMode.STREAMING);
+
         Properties props = new Properties();
         props.setProperty("bootstrap.servers", "localhost:9092");
         props.setProperty("group.id", "flink-group");
+        props.setProperty("max.poll.records", "10"); // Read only 1000 records meaning a batch
 
-        FlinkKafkaConsumer<String> consumer = new FlinkKafkaConsumer<>("bootstrap_topic", new SimpleStringSchema(), props);
+
+        // Kafka message likes this: 1 2 3 4 5 6 7 8 9 10
+        FlinkKafkaConsumer<String> consumer = new FlinkKafkaConsumer<>("bootstrap_topic", new SimpleStringSchema(),
+                props);
         env.addSource(consumer)
-                .map(new MapFunction<String, String>() {
+                .map((MapFunction<String, Integer>) Integer::parseInt)
+                .map(new RichMapFunction<Integer, Integer>() {
+                    private ValueState<Integer> state;
+
                     @Override
-                    public String map(String value) throws Exception {
-                        return value.split(",")[0];  // Extract user ID
+                    public void open(Configuration parameters) {
+                        ValueStateDescriptor<Integer> descriptor =
+                                new ValueStateDescriptor<>("state", TypeInformation.of(Integer.class));
+                        state = getRuntimeContext().getState(descriptor);
+                    }
+
+                    @Override
+                    public Integer map(Integer value) throws Exception {
+                        Integer currentState = state.value();
+                        if (currentState != null) {
+                            System.out.println("Current state: " + currentState);
+                            // Update the state
+                            state.update(currentState + value);
+                            return currentState + value;
+                        } else {
+                            // Initialize the state if it's null
+                            state.update(value);
+                            return value;
+                        }
                     }
                 })
-                .print();  // For demonstration, you can write to any sink
+                .print();
 
         env.execute("Bounded Job");
     }
