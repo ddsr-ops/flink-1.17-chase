f0749413a3cb74ba76ba914dac968004889c47f9
gongchanghua <759444207@qq.com>
2024-11-21 11:25:46 +0800

    feature(state):  invocation of CountWindowAverage

--- src/main/java/com/ddsr/state/CountWindowAverage.java
+++ src/main/java/com/ddsr/state/CountWindowAverage.java
@@ -7,11 +7,14 @@ import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
 /**
  * @author ddsr, created it at 2024/11/21 11:13
  */
+
+@SuppressWarnings("deprecation")
 public class CountWindowAverage extends RichFlatMapFunction<Tuple2<Long, Long>, Tuple2<Long, Long>> {
     /**
      * The ValueState handle. The first field is the count, the second field a running sum.
@@ -49,9 +52,20 @@ public class CountWindowAverage extends RichFlatMapFunction<Tuple2<Long, Long>, 
         sum = getRuntimeContext().getState(descriptor);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+        env.setParallelism(1);
 
+        // this can be used in a streaming program like this (assuming we have a StreamExecutionEnvironment env)
+        env.fromElements(Tuple2.of(1L, 3L), Tuple2.of(1L, 5L), Tuple2.of(1L, 7L), Tuple2.of(1L, 4L), Tuple2.of(1L, 2L))
+                .keyBy(value -> value.f0)
+                .flatMap(new CountWindowAverage())
+                .print();
+
+        // the printed output will be (1,4) and (1,5)
+
+        env.execute();
 
 
     }
