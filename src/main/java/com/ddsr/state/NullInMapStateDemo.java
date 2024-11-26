484db04a72abbf870d1ff099a28c2bd218069c1b
gongchanghua <759444207@qq.com>
2024-11-26 18:14:21 +0800

    feature(state): Invoke the MapStateDescriptor with NullableSerializer

--- src/main/java/com/ddsr/state/NullInMapStateDemo.java
+++ src/main/java/com/ddsr/state/NullInMapStateDemo.java
@@ -4,15 +4,20 @@ import org.apache.flink.api.common.state.MapState;
 import org.apache.flink.api.common.state.MapStateDescriptor;
 import org.apache.flink.api.common.state.StateTtlConfig;
 import org.apache.flink.api.common.time.Time;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeutils.base.IntSerializer;
 import org.apache.flink.api.common.typeutils.base.StringSerializer;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.typeutils.runtime.NullableSerializer;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.util.Collector;
+
+import java.util.Arrays;
 
 /**
  * The map state with TTL currently supports null user values only if the user value serializer can handle null values.
@@ -26,13 +31,18 @@ public class NullInMapStateDemo {
         // Set up the execution environment
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        // Sample data stream
-        DataStream<Tuple2<String, Integer>> input = env.fromElements(
-                Tuple2.of("key1", 1),
-                Tuple2.of("key2", 2),
-                Tuple2.of("key1", null), // Null value example
-                Tuple2.of("key2", 3)
-        );
+        // Define custom TypeInformation for the input data
+        TypeInformation<Tuple2<String, Integer>> typeInfo = TypeInformation.of(new TypeHint<Tuple2<String, Integer>>() {
+        });
+
+        // Sample data stream with custom TypeInformation
+        DataStreamSource<Tuple2<String, Integer>> input = env.fromCollection(
+                Arrays.asList(
+                        Tuple2.of("key1", 1),
+                        Tuple2.of("key2", 2),
+                        Tuple2.of("key1", null), // Null value example
+                        Tuple2.of("key2", 3)),
+                typeInfo);
 
         // Apply a transformation with stateful processing
         DataStream<String> result = input
