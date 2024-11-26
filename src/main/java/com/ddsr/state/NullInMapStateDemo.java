adccbe310fcb63db107e89e567c54dccc993b463
gongchanghua <759444207@qq.com>
2024-11-26 18:19:18 +0800

    feature(state): Invoke the MapStateDescriptor with NullableSerializer

--- src/main/java/com/ddsr/state/NullInMapStateDemo.java
+++ src/main/java/com/ddsr/state/NullInMapStateDemo.java
@@ -4,25 +4,24 @@ import org.apache.flink.api.common.state.MapState;
 import org.apache.flink.api.common.state.MapStateDescriptor;
 import org.apache.flink.api.common.state.StateTtlConfig;
 import org.apache.flink.api.common.time.Time;
-import org.apache.flink.api.common.typeinfo.TypeHint;
-import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeutils.base.IntSerializer;
 import org.apache.flink.api.common.typeutils.base.StringSerializer;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.typeutils.runtime.NullableSerializer;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.streaming.api.functions.source.RichSourceFunction;
 import org.apache.flink.util.Collector;
-
-import java.util.Arrays;
 
 /**
  * The map state with TTL currently supports null user values only if the user value serializer can handle null values.
  * If the serializer does not support null values, it can be wrapped with NullableSerializer at the cost of an extra
  * byte in the serialized form
+ * <p>
+ *
+ * org.apache.flink.types.NullFieldException: Field 1 is null, but expected to hold a value.
  *
  * @author ddsr, created it at 2024/11/25 18:10
  */
@@ -31,18 +30,8 @@ public class NullInMapStateDemo {
         // Set up the execution environment
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        // Define custom TypeInformation for the input data
-        TypeInformation<Tuple2<String, Integer>> typeInfo = TypeInformation.of(new TypeHint<Tuple2<String, Integer>>() {
-        });
-
-        // Sample data stream with custom TypeInformation
-        DataStreamSource<Tuple2<String, Integer>> input = env.fromCollection(
-                Arrays.asList(
-                        Tuple2.of("key1", 1),
-                        Tuple2.of("key2", 2),
-                        Tuple2.of("key1", null), // Null value example
-                        Tuple2.of("key2", 3)),
-                typeInfo);
+        // Sample data stream using a custom source function
+        DataStream<Tuple2<String, Integer>> input = env.addSource(new CustomSourceFunction());
 
         // Apply a transformation with stateful processing
         DataStream<String> result = input
@@ -55,6 +44,25 @@ public class NullInMapStateDemo {
         // Execute the job
         env.execute("Flink Map State with TTL Example");
 
+    }
+
+    // Custom source function to emit elements, including null values
+    public static class CustomSourceFunction extends RichSourceFunction<Tuple2<String, Integer>> {
+        private volatile boolean isRunning = true;
+
+        @Override
+        public void run(SourceContext<Tuple2<String, Integer>> ctx) throws Exception {
+            ctx.collect(Tuple2.of("key1", 1));
+            ctx.collect(Tuple2.of("key2", 2));
+            ctx.collect(Tuple2.of("key1", null)); // Null value example
+            ctx.collect(Tuple2.of("key2", 3));
+            isRunning = false;
+        }
+
+        @Override
+        public void cancel() {
+            isRunning = false;
+        }
     }
 
     public static class StatefulMapFunction extends KeyedProcessFunction<String, Tuple2<String, Integer>, String> {
