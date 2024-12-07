fdc78f1ea7c02098f3b284fcbf33322f25e0046a
gongchanghua <759444207@qq.com>
2024-12-07 20:27:52 +0800

    feature(state): [broadcast-state-pattern] add a shape enum class

---
+++ src/main/java/com/ddsr/state/examples/Item.java
@@ -0,0 +1,7 @@
+package com.ddsr.state.examples;
+
+/**
+ * @author ddsr, created it at 2024/12/7 20:25
+ */
+public class Item {
+}
---
+++ src/main/java/com/ddsr/state/examples/Shape.java
@@ -0,0 +1,9 @@
+package com.ddsr.state.examples;
+
+/**
+ * @author ddsr, created it at 2024/12/7 20:25
+ */
+// Shape enum (presuming Shape is an enum)
+public enum Shape {
+    CIRCLE, SQUARE, TRIANGLE
+}
