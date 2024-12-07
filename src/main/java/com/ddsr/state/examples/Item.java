3ad4dff45351c35ab4112a49e36152c8d50662d4
gongchanghua <759444207@qq.com>
2024-12-07 20:28:33 +0800

    feature(state): [broadcast-state-pattern] add an item class

--- src/main/java/com/ddsr/state/examples/Item.java
+++ src/main/java/com/ddsr/state/examples/Item.java
@@ -3,5 +3,39 @@ package com.ddsr.state.examples;
 /**
  * @author ddsr, created it at 2024/12/7 20:25
  */
+// Item class
 public class Item {
+    private String id;
+    private Shape shape;
+
+    public Item() { }
+
+    public Item(String id, Shape shape) {
+        this.id = id;
+        this.shape = shape;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Shape getShape() {
+        return shape;
+    }
+
+    public void setShape(Shape shape) {
+        this.shape = shape;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id='" + id + '\'' +
+                ", shape=" + shape +
+                '}';
+    }
 }
