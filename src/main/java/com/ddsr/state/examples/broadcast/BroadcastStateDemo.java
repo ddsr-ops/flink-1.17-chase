2094ca5d1f627e35778a234367a458da6e5e2d16
gongchanghua <759444207@qq.com>
2024-12-09 10:50:10 +0800

    feature(state): [broadcast-state-pattern] add a complete implementation

--- src/main/java/com/ddsr/state/examples/broadcast/BroadcastStateDemo.java
+++ src/main/java/com/ddsr/state/examples/broadcast/BroadcastStateDemo.java
@@ -1,5 +1,6 @@
 package com.ddsr.state.examples.broadcast;
 
+import org.apache.flink.api.common.state.MapState;
 import org.apache.flink.api.common.state.MapStateDescriptor;
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
 import org.apache.flink.api.common.typeinfo.TypeHint;
@@ -13,6 +14,10 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.co.KeyedBroadcastProcessFunction;
 import org.apache.flink.util.Collector;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * we have a stream of objects of different colors and shapes and we want to find pairs of objects of the same color
  * that follow a certain pattern, e.g. a rectangle followed by a triangle. We assume that the set of interesting
@@ -23,6 +28,7 @@ import org.apache.flink.util.Collector;
  * href="https://nightlies.apache.org/flink/flink-docs-release-1.20/docs/dev/datastream/fault-tolerance/broadcast_state/"
  * >broadcast_state</a>
  */
+@SuppressWarnings({"Convert2Lambda", "Anonymous2MethodRef"})
 public class BroadcastStateDemo {
     public static void main(String[] args) throws Exception {
 
@@ -32,6 +38,7 @@ public class BroadcastStateDemo {
         SingleOutputStreamOperator<Item> itemStream = env.socketTextStream("192.168.20.126", 7777)
                 .map(r -> {
                     String[] fields = r.split(",");
+                    // color, shape
                     return new Item(Color.valueOf(fields[2]), Shape.valueOf(fields[1]));
                 });
 
@@ -45,18 +52,19 @@ public class BroadcastStateDemo {
                 });
 
 
-        // key the items by color
+        // key the items by color, items with the same color go to the same partition
         KeyedStream<Item, Color> colorPartitionedStream = itemStream
                 .keyBy(new KeySelector<Item, Color>() {
 
                     @Override
-                    public Color getKey(Item value) throws Exception {
+                    public Color getKey(Item value) {
                         return value.getColor();
                     }
                 });
 
 
         // a map descriptor to store the name of the rule (string) and the rule itself.
+        // declare a state descriptor to broadcast
         MapStateDescriptor<String, Rule> ruleStateDescriptor = new MapStateDescriptor<>(
                 "RulesBroadcastState",
                 BasicTypeInfo.STRING_TYPE_INFO,
@@ -67,20 +75,96 @@ public class BroadcastStateDemo {
         BroadcastStream<Rule> ruleBroadcastStream = ruleStream
                 .broadcast(ruleStateDescriptor);
 
+        KeyedBroadcastProcessFunction<Color, Item, Rule, String> keyedBroadcastProcessFunction =
+                new KeyedBroadcastProcessFunction<Color, Item, Rule, String>() {
+
+            // store rules to match, the first type parameter is rule name, the second one is rule
+            private final MapStateDescriptor<String, Rule> ruleStateDescriptor =
+                    new MapStateDescriptor<>(
+                            "RulesBroadcastState",
+                            BasicTypeInfo.STRING_TYPE_INFO,
+                            TypeInformation.of(new TypeHint<Rule>() {
+                            }));
+
+            // store partial matches, i.e. first elements of the pair waiting for the corresponding
+            // second element
+            // keep a list as we have many first elements waiting, the first type parameter is rule
+            // name used for joining the above rule state, the second one is list of first elements
+            private final MapStateDescriptor<String, List<Item>> firstElementsStateDescriptor =
+                    new MapStateDescriptor<>(
+                            "FirstElementsBroadcastState",
+                            BasicTypeInfo.STRING_TYPE_INFO,
+                            TypeInformation.of(new TypeHint<List<Item>>() {
+                            }));
+
+                    @Override
+                    public void processElement(Item value, ReadOnlyContext ctx, Collector<String> out) throws Exception {
+                        // readonlycontext can only read the broadcast state, not modify it
+                        final Shape currentShape = value.getShape();
+
+                        // get state from firstElementsStateDescriptor, the key of map is rule name
+                        final MapState<String, List<Item>> firstElementsKeyedState =
+                                getRuntimeContext().getMapState(firstElementsStateDescriptor);
+
+                        // loop the rule state from broadcast state
+                        for (Map.Entry<String, Rule> entry :
+                                ctx.getBroadcastState(ruleStateDescriptor).immutableEntries()) {
+                            // get the rule
+                            final Rule rule = entry.getValue();
+                            // get the first shape
+                            final Shape firstShape = rule.first;
+                            // get the second shape
+                            final Shape secondShape = rule.second;
+                            // rule name
+                            final String ruleName = entry.getKey();
+
+                            List<Item> storedFirstItems = firstElementsKeyedState.get(ruleName);
+                            if (storedFirstItems == null) {
+                                // if there is no first elements stored, create an empty list
+                                storedFirstItems = new ArrayList<>();
+                            }
+
+                            // matched
+                            if(currentShape == secondShape &&  !storedFirstItems.isEmpty()) {
+                                // if the current shape is the same as the second shape, and there is
+                                // already a first element stored, then we have a match
+                                for (Item firstItem : storedFirstItems) {
+                                    out.collect(ruleName + " matches " + firstItem.getColor() + " " + firstItem.getShape() + " with " + value.getColor() + " " + value.getShape());
+                                }
+                            }
+
+
+                            if(currentShape == firstShape) {
+                                // if the current shape is the same as the first shape, then we store
+                                // the current item
+                                storedFirstItems.add(value);
+                                // todo: the storedFirstItems might have multiple first elements, occupying much
+                                //  state size
+                            }
+
+                            if (storedFirstItems.isEmpty()) {
+                                firstElementsKeyedState.remove(ruleName);
+                            } else {
+                                firstElementsKeyedState.put(ruleName, storedFirstItems);
+                            }
+
+
+                        }
+                    }
+
+            @Override
+            public void processBroadcastElement(Rule value, Context ctx, Collector<String> out) throws Exception {
+                // store the rule
+                ctx.getBroadcastState(ruleStateDescriptor).put(value.name, value);
+
+            }
+        };
+
         colorPartitionedStream
                 .connect(ruleBroadcastStream)
                 .process(
-                        new KeyedBroadcastProcessFunction<Color, Item, Rule, String>() {
-                            @Override
-                            public void processElement(Item value, ReadOnlyContext ctx, Collector<String> out) throws Exception {
 
-                            }
-
-                            @Override
-                            public void processBroadcastElement(Rule value, Context ctx, Collector<String> out) throws Exception {
-
-                            }
-                        }
+                        keyedBroadcastProcessFunction
                 );
 
 
