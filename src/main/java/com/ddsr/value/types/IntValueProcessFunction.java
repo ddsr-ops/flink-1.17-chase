cf8a0d06835d871e39ab78d874bcced42da0dab7
gongchanghua <759444207@qq.com>
2024-12-19 09:41:58 +0800

    feature(state): [value-type] value types reduce garbage collection pressure by reusing objects

---
+++ src/main/java/com/ddsr/value/types/IntValueProcessFunction.java
@@ -0,0 +1,20 @@
+package com.ddsr.value.types;
+
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.types.IntValue;
+import org.apache.flink.util.Collector;
+
+/**
+ * Using mutable Value types like IntValue, DoubleValue, etc., can help reduce garbage collection pressure by reusing
+ * objects. This is especially beneficial in high-throughput streaming applications where object creation and garbage
+ * collection can become bottlenecks.
+ *
+ * @author ddsr, created it at 2024/12/19 9:38
+ */
+public class IntValueProcessFunction extends ProcessFunction<Integer, IntValue> { // in, out
+    @Override
+    public void processElement(Integer input, Context ctx, Collector<IntValue> out) throws Exception {
+        IntValue intValue = new IntValue(input);
+        out.collect(intValue);
+    }
+}
