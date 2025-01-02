54a90a0dff81a79ff756a9123fd4e97e5b9d95ce
gongchanghua <759444207@qq.com>
2025-01-02 17:13:48 +0800

    feature(state): add a product class to illustrate the separation of TypeSerializerSnapshot

---
+++ src/main/java/com/ddsr/serialization/Product.java
@@ -0,0 +1,31 @@
+package com.ddsr.serialization;
+
+import java.util.Objects;
+
+/**
+ * @author ddsr, created it at 2025/1/2 17:11
+ */
+public class Product {
+    public String name;
+    public double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product() {}
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Product product = (Product) object;
+        return Double.compare(price, product.price) == 0 && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+}
