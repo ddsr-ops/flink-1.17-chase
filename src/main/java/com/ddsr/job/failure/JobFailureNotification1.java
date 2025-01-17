84f363fdcf40021e61939250384ed6226bb1c965
gongchanghua <759444207@qq.com>
2025-01-17 10:47:00 +0800

    feature(job-failure): a notification mechanism if the job failed

--- src/main/java/com/ddsr/job/failure/JobFailureNotification1.java
+++ src/main/java/com/ddsr/job/failure/JobFailureNotification1.java
@@ -1,24 +1,34 @@
 package com.ddsr.job.failure;
 
 import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
- * This demo illustrates that notification should be broadcasted to relevant services if the job fails. This is a
- * finite datestream demo.
+ * This demo illustrates that notification should be broadcasted to relevant services if the job fails. This is an
+ * infinite datestream demo.
  *
  *
  * @author ddsr, created it at 2025/1/17 10:20
  */
-public class JobFailureNotification {
+public class JobFailureNotification1 {
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         env.setParallelism(1);
 
-        // Element zero mocks the application failure
-        env.fromElements(0, 1, 2, 3, 4)
-                .map(x -> 2 / x)
+        // Three zeros can make the job failed
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, 1000));
+
+        // Infinite date stream, zero should be populated to mock an application failure
+        /*
+           nc -lk 7777
+           1
+           2
+           0 --> mock an application failure, which incurs the job failure
+         */
+        env.socketTextStream("192.168.20.126", 7777)
+                .map(x -> 2 / Long.parseLong(x))
                 .print();
 
         try {
@@ -30,7 +40,7 @@ public class JobFailureNotification {
             throw new RuntimeException(e);
         }
 
-        // This line is not reachable if exceptions occurred
+        // This line is not reachable if exceptions occurred(throw new RuntimeException(e))
         System.out.println("The line after try-catch block");
 
 
